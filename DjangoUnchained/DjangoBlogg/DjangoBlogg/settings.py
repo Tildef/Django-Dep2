60da4873b83adc822e37ef40910b853db573ab75
Tildef <46127@gymnasium.ax>
2023-01-20 11:09:55 +0200

    Fixat filer

--- DjangoUnchained/DjangoBlogg/DjangoBlogg/settings.py
+++ DjangoUnchained/DjangoBlogg/DjangoBlogg/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-dzva$k_a5z(hn)26vl*uyoq68ooaa!n!$s2t8$yrhtjzb7qzgt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['http://localhost:8000/','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8000/']
 
 # Application definition
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
