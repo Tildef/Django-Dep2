e7853428eeb0a381844a60c6639f8dc1da5e7d07
Tildef <46127@gymnasium.ax>
2023-01-23 13:14:44 +0200

    Fixat requirements filen

--- DjangoBlogg/DjangoBlogg/settings.py
+++ DjangoBlogg/DjangoBlogg/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-dzva$k_a5z(hn)26vl*uyoq68ooaa!n!$s2t8$yrhtjzb7qzgt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://localhost:8000/','127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
+#'http://localhost:8000/','127.0.0.1:8000'
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000/']
 
 # Application definition
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
